b186db7c4f720cde1a30e5924a42dca77febe6d9
Fomin Nickolai <giraffe.in.ua@gmail.com>
2017-09-22 13:38:15 +0300

    Added another SCNNode relatively positioned.

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -58,34 +58,24 @@ class ViewController: UIViewController {
 //        path.addLine(to: CGPoint(x: 0.2, y: 0.3))
 //        path.addLine(to: CGPoint(x: 0.4, y: 0.2))
 //        path.addLine(to: CGPoint(x: 0.4, y: 0))
-        
-        //// Color Declarations
-        
-        //// Star Drawing
-        let starPath = UIBezierPath()
-        starPath.move(to: CGPoint(x: 0.5, y: 0))
-        starPath.addLine(to: CGPoint(x: 0.69, y: 0.28))
-        starPath.addLine(to: CGPoint(x: 1, y: 0.38))
-        starPath.addLine(to: CGPoint(x: 0.8, y: 0.66))
-        starPath.addLine(to: CGPoint(x: 0.81, y: 1))
-        starPath.addLine(to: CGPoint(x: 0.5, y: 0.88))
-        starPath.addLine(to: CGPoint(x: 0.19, y: 1))
-        starPath.addLine(to: CGPoint(x: 0.2, y: 0.66))
-        starPath.addLine(to: CGPoint(x: 0, y: 0.38))
-        starPath.addLine(to: CGPoint(x: 0.31, y: 0.28))
-        starPath.addLine(to: CGPoint(x: 0.5, y: 0))
-        starPath.close()
-        
-        let shape = SCNShape(path: starPath, extrusionDepth: 0.1)
-        
-        node.geometry = shape
+        //let x = randomNumbers(fristNum: -0.3, secondNum: 0.3)
+        //let y = randomNumbers(fristNum: -0.3, secondNum: 0.3)
+        //let z = randomNumbers(fristNum: -0.3, secondNum: 0.3)
+
+        node.geometry = SCNPyramid(width: 0.1, height: 0.1, length: 0.1)
         node.geometry?.firstMaterial?.specular.contents = UIColor.orange
         node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
-        let x = randomNumbers(fristNum: -0.3, secondNum: 0.3)
-        let y = randomNumbers(fristNum: -0.3, secondNum: 0.3)
-        let z = randomNumbers(fristNum: -0.3, secondNum: 0.3)
-        node.position = SCNVector3(x, y, z)
+        node.position = SCNVector3(0.2, 0.3, -0.2)
         self.sceneView.scene.rootNode.addChildNode(node)
+        
+        let cylinderNode = SCNNode()
+        cylinderNode.geometry = SCNCylinder(radius: 0.1, height: 0.2)
+        cylinderNode.geometry?.firstMaterial?.specular.contents = UIColor.orange
+        cylinderNode.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        cylinderNode.position = SCNVector3(-0.3, 0.2, -0.3)
+        node.addChildNode(cylinderNode)
+        
+        
         
     }
     

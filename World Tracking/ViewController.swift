7b328acce8969a65f92d01dcebd9cf53824ac002
Fomin Nickolai <giraffe.in.ua@gmail.com>
2017-09-22 11:46:50 +0300

    Added method for restart session.

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -20,13 +20,26 @@ class ViewController: UIViewController {
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         self.sceneView.session.run(configuration)
     }
-
+    
+    func restartSession() {
+        self.sceneView.session.pause()
+        self.sceneView.scene.rootNode.enumerateChildNodes { (node, _) in
+            node.removeFromParentNode()
+        }
+        self.sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+    }
+    
+    @IBAction func resetPressed(_ sender: UIButton) {
+        self.restartSession()
+        
+    }
+    
     @IBAction func addPressed(_ sender: UIButton) {
         
         let node = SCNNode()
         node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
         node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
-        node.position = SCNVector3(-0.1, -0.1, -0.1)
+        node.position = SCNVector3(0, 0, -0.3)
         self.sceneView.scene.rootNode.addChildNode(node)
         
     }

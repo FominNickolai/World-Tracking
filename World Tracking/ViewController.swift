4dada22b718affd5df434133d746e7c15692cd36
Fomin Nickolai <giraffe.in.ua@gmail.com>
2017-09-22 12:05:45 +0300

    Added button for reset current session.

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -11,16 +11,22 @@ import ARKit
 
 class ViewController: UIViewController {
 
+    //MARK: - @IBOutets
     @IBOutlet weak var sceneView: ARSCNView!
     
+    //MARK: - Properties
     let configuration = ARWorldTrackingConfiguration()
     
+    //MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         self.sceneView.session.run(configuration)
+        
+        self.sceneView.autoenablesDefaultLighting = true
     }
     
+    //MARK: - Restart Session
     func restartSession() {
         self.sceneView.session.pause()
         self.sceneView.scene.rootNode.enumerateChildNodes { (node, _) in
@@ -29,6 +35,7 @@ class ViewController: UIViewController {
         self.sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
     }
     
+    //MARK: - @IBActions
     @IBAction func resetPressed(_ sender: UIButton) {
         self.restartSession()
         
@@ -37,11 +44,29 @@ class ViewController: UIViewController {
     @IBAction func addPressed(_ sender: UIButton) {
         
         let node = SCNNode()
-        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.03)
+        node.geometry?.firstMaterial?.specular.contents = UIColor.orange
         node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
-        node.position = SCNVector3(0, 0, -0.3)
+        let x = randomNumbers(fristNum: -0.3, secondNum: 0.3)
+        let y = randomNumbers(fristNum: -0.3, secondNum: 0.3)
+        let z = randomNumbers(fristNum: -0.3, secondNum: 0.3)
+        node.position = SCNVector3(x, y, z)
         self.sceneView.scene.rootNode.addChildNode(node)
         
     }
+    
+    //MARK: - Helpers Methods
+    //Random value in Range
+    func randomNumbers(fristNum: CGFloat, secondNum: CGFloat) -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(fristNum - secondNum) + min(fristNum, secondNum)
+    }
 }
+
+
+
+
+
+
+
+
 
